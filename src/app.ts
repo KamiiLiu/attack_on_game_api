5fee373e31ad5e51bad875826405e0cfbd95dd05
personal <k01midy@gmail.com>
2024-05-06 02:49:49 +0800

    feat:確保存欓格式都漂漂亮亮

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ const app = express();
 app.use(express.json());
 console.log('你安装成功哩QQ');
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
-  });
+  res.send('Hello, World!');
+});
 export default app;
-
