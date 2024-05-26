b058c1972e3fe89be329336b2d816cdae2af4525
personal <k01midy@gmail.com>
2024-05-26 16:59:46 +0800

    feat:example-code

---
+++ src/routers/event.ts
@@ -0,0 +1,10 @@
+// import { Router } from 'express';
+// import { handleValidationErrors } from '@/middlewares/handleValidationErrors';
+// import eventValidator from '@/validators/eventValidator';
+// const router = Router();
+// router.post(
+//   '/',
+//   eventValidator,
+//   handleValidationErrors,
+//   EventController.createEvent,
+// );
